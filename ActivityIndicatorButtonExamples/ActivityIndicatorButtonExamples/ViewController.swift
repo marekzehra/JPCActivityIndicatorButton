9f8052042810c31d937a3caede481d155ece8c0d
Jon Chmura <jon.chmura@me.com>
2015-04-14 11:39:43 -0400

    Improved example view controller

--- ActivityIndicatorButtonExamples/ActivityIndicatorButtonExamples/ViewController.swift
+++ ActivityIndicatorButtonExamples/ActivityIndicatorButtonExamples/ViewController.swift
@@ -12,12 +12,36 @@ class ViewController: UIViewController {
 
     @IBOutlet var activityIndicator: ActivityIndicatorButton!
     
+    @IBOutlet var stateSelector: UISegmentedControl!
+    @IBOutlet var solidButtonSwitch: UISwitch!
+    @IBOutlet var progressSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.activityIndicator.useSolidColorButtons = true
+        self.activityIndicator.progress = 0.45
         self.activityIndicator.setTrackColor(UIColor.lightGrayColor(), forActivityStates: [.Spinning, .Progress])
         self.activityIndicator.setTintColor(UIColor(red: 0.298, green: 0.686, blue: 0.314, alpha: 1.0), forActivityStates: [.Complete])
+        
+        self.solidButtonSwitch.on = self.activityIndicator.useSolidColorButtons
+        self.progressSlider.value = self.activityIndicator.progress
+    }
+    
+    
+    
+    // MARK: Activity Button
+    
+    func nextState(state: ActivityIndicatorButton.ActivityState) -> ActivityIndicatorButton.ActivityState {
+        switch state {
+        case .Inactive: return .Spinning
+        case .Spinning: return .Progress
+        case .Progress: return .Paused
+        case .Paused: return .Progress
+        case .Complete: return .Inactive
+        }
     }
     
     @IBAction func touchDown(sender: AnyObject) {
@@ -46,12 +70,40 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpInside(sender: AnyObject) {
         println("TOUCH UP INSIDE")
+        
+        let state = nextState(self.activityIndicator.activityState)
+        self.activityIndicator.transition(toActivityState: state, animated: true)
+        self.setSliderActivityState(state)
     }
     
     @IBAction func touchUpOutside(sender: AnyObject) {
         println("TOUCH UP OUTSIDE")
     }
 
+    
+    
+    // MARK: Controls
+    
+    func setSliderActivityState(state: ActivityIndicatorButton.ActivityState) {
+        switch state {
+        case .Inactive:
+            self.stateSelector.selectedSegmentIndex = 0
+        case .Spinning:
+            self.stateSelector.selectedSegmentIndex = 1
+        case .Progress:
+            self.stateSelector.selectedSegmentIndex = 2
+        case .Paused:
+            self.stateSelector.selectedSegmentIndex = 3
+        case .Complete:
+            self.stateSelector.selectedSegmentIndex = 4
+        }
+    }
+    
+    @IBAction func solidButtonChanged(sender: UISwitch) {
+        self.activityIndicator.useSolidColorButtons = sender.on
+        self.activityIndicator.transition(toActivityState: self.activityIndicator.activityState, animated: false)
+    }
+    
     @IBAction func stateValueChanged(sender: UISegmentedControl) {
         
         var newState: ActivityIndicatorButton.ActivityState!
