e50461393b02b0f6970e235fb12f0187f6d32561
Jon Chmura <jon.chmura@me.com>
2015-07-06 10:41:15 -0400

    Added project to build into a framework.

---
+++ JPCActivityIndicatorButton/JPCActivityIndicatorButton/JPCActivityIndicatorButton.h
@@ -0,0 +1,19 @@
+//
+//  JPCActivityIndicatorButton.h
+//  JPCActivityIndicatorButton
+//
+//  Created by Jon Chmura on 7/6/15.
+//  Copyright (c) 2015 Jon Chmura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JPCActivityIndicatorButton.
+FOUNDATION_EXPORT double JPCActivityIndicatorButtonVersionNumber;
+
+//! Project version string for JPCActivityIndicatorButton.
+FOUNDATION_EXPORT const unsigned char JPCActivityIndicatorButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JPCActivityIndicatorButton/PublicHeader.h>
+
+
