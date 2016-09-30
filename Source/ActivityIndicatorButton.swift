7dde7b6b452365af17e86e69ad53c4d449d5a13d
Marek Žehra <marek@zehra.cz>
2016-09-30 23:12:42 +0200

    Update ActivityIndicatorButton.swift

--- Source/ActivityIndicatorButton.swift
+++ Source/ActivityIndicatorButton.swift
@@ -922,7 +922,7 @@ public class ActivityIndicatorButton: UIControl {
 
     private func setImage(image: UIImage?) {
         self.imageView.image = image
-        self.imageView.sizeToFit()
+        self.imageView.contentMode = .Center
     }
 
 
